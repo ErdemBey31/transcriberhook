1011eb0c1c1edcdf39d6ec676140cabe335b8483
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2024-04-05 06:46:21 +0300

    Add files via upload

--- index.py
+++ index.py
@@ -1,77 +1,45 @@
-from flask import Flask, request
-import assemblyai as aai
-import os
+from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
+token = "6422272052:AAEBkx3UANSWYelsjFnyP2e2YnEY6Pg7zEk"
 
-aai.settings.api_key = "059b26e05fda420a9612d5aeb4bad83f"
-config = aai.TranscriptionConfig(language_code="tr")
-transcriber = aai.Transcriber(config=config)
-
-bot_token = "6765509934:AAGIpSWslGRGlLlvWgjHg3q02f3QOnYdOFI"
-YOUR_BOT_TOKEN = bot_token
-
-@app.route('/webhook', methods=['POST'])
+@app.route('/bot', methods=['POST'])
 def webhook():
     data = request.get_json()
-    message = data.get('message')
-    if message and 'text' in message:
-        text = message['text']
-        if text == '/start':
-            send_start_message(message)
-    elif message and 'voice' in message:
-        voice_message = message['voice']
-        file_id = voice_message['file_id']
-        file_path = download_file(file_id)
-        transcribe_audio(file_path)
-        remove_file(file_path)
-    return 'Webhook received successfully'
-
-def send_start_message(message):
+    message = data['message']
     chat_id = message['chat']['id']
-    start_message = """
-    👋 Merhaba! Transcriber Bot'a hoşgeldin.
+    text = get_message_text(message)
 
-    📝 Bu bot, grubunuzda gönderdiğiniz sesli mesajları metin haline çevirir.
+    if text.startswith('/start'):
+        send_message(chat_id, "*Çevirmek için mesaj gönder!!*")
+    else:
+        translation = translate_text(text)
+        send_message(chat_id, f"{translation}")
 
-    Sesli mesaj göndermek için sadece bir ses kaydediciye tıklayın ve sesli mesajı gönderin.
+    return jsonify({'success': True})
 
-    Başlamak için /start komutunu kullanabilirsiniz.
-    """
-    send_message(chat_id, start_message)
+def get_message_text(message):
+    if 'caption' in message:
+        return message['caption']
+    elif 'text' in message:
+        return message['text']
+    else:
+        return ''
+
+def translate_text(text):
+    response = requests.get(f"""https://codingllama.codingteamapi.workers.dev/?token=CTAPI-038AXEXYbdOjvRotvRCPXWiBvv&question=Merhaba! Çeviri konusunda yardımınıza ihtiyacım var. Auto-dedected bir metni Türkçeye çevirmeniz mümkün mü? Metni aşağıda bulabilirsiniz:
+
+{text}
+Çevirinizi bekliyor olacağım. Teşekkür ederim!"""\n\n{text}")
+    translation = response.json().get('bot')
+    return translation
 
 def send_message(chat_id, text):
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    data = {
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    params = {
         'chat_id': chat_id,
-        'text': text
+        'text': text,
+        'parse_mode': 'markdown'
     }
-    response = requests.post(url, json=data)
-    if response.status_code != 200:
-        print(f"Failed to send message. Error: {response.text}")
-
-def download_file(file_id):
-    
-    file_url = f"https://api.telegram.org/bot{YOUR_BOT_TOKEN}/getFile?file_id={file_id}"
-    response = requests.get(file_url)
-    if response.status_code == 200:
-        file_data = response.json()
-        file_path = file_data.get('result', {}).get('file_path')
-        if file_path:
-            file_url = f"https://api.telegram.org/file/bot{YOUR_BOT_TOKEN}/{file_path}"
-            response = requests.get(file_url)
-            if response.status_code == 200:
-                os.listdir(file_path)
-                with open(file_path, 'w+') as file:
-                    file.write(response.content)
-    return file_path
-
-def transcribe_audio(file_path):
-    trans = transcriber.transcribe(file_path)
-    # Process the transcription result as per your requirements.
-    # You can send it as a response or take any other action.
-    print(trans.text)
-
-def remove_file(file_path):
-    os.remove(file_path)
+    requests.post(url, json=params)
